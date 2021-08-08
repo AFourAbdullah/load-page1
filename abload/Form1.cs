0e0d68ad34c81375f20a7464f2365f473896c41a
Muhammad Abdullah <Muhammad Abdullah@LAPTOP-6RLPLBCF>
2021-08-08 15:12:25 +0500

    initial

---
+++ abload/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel3.Width += 2;
+            if (panel3.Width >= 835)
+            {
+                timer1.Stop();
+                Form2 f = new Form2();
+                f.Show();
+                this.Hide();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
